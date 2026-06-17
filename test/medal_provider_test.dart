ccfab4b3adbd3345a9db3a9a86896b3de4a18f45
lhy126a <terry.lee3@ha.org.hk>
2026-06-17 09:21:49 +0800

    Fix wrong import path in MedalProvider test; refresh analysis_options comment
    - test/medal_provider_test.dart: package name in pubspec.yaml is
      'earth_walker' (underscore), not 'earthwalker'. The import was wrong
      and would not resolve.
    - analysis_options.yaml: comment block still referenced
      'flutter_lints/flutter.yaml' even though the include was switched
      to package:lint/analysis_options.yaml. Refresh the comment.

--- test/medal_provider_test.dart
+++ test/medal_provider_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:earthwalker/providers/medal_provider.dart';
+import 'package:earth_walker/providers/medal_provider.dart';
 
 void main() {
   test('MedalProvider exposes the 7 medals in order', () {
